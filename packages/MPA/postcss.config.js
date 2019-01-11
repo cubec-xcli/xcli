9e24faa14f4badbae28986c4c2a2b39e53fb93ed
YiJun <cloudywolf1@qq.com>
2019-01-11 22:18:23 +0800

    增加mpa 支持多页面打包，完美

--- packages/MPA/postcss.config.js
+++ packages/MPA/postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   plugins: [
-    require(require.resolve("autoprefixer"))({
+    require(require.resolve("postcss-preset-env"))({
       browsers: ['last 2 versions', 'iOS >= 6', "Android >= 4.4", "not ie <= 10"]
     }),
-    require(require.resolve("postcss-preset-env"))({}),
     require(require.resolve("postcss-pxtorem"))({
       rootValue: 37.5,
       unitPrecision: 5,
