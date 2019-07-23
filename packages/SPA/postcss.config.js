e229a6226756e8febdfa9f21a4b105786a88361e
YiJun <cloudywolf1@qq.com>
2019-07-23 18:27:08 +0800

    delete postcss-momentum-scrolling

--- packages/SPA/postcss.config.js
+++ packages/SPA/postcss.config.js
@@ -3,7 +3,7 @@ const postcss_preset = require(require.resolve("postcss-preset-env"))({
   // browsers: ['last 2 versions', 'iOS >= 6', "Android >= 4.3", "not ie <= 10"]
 });
 const postcss_clean = require(require.resolve("postcss-clean"))({ level: 2 });
-const postcss_momentum_scrolling = require(require.resolve("postcss-momentum-scrolling"));
+// const postcss_momentum_scrolling = require(require.resolve("postcss-momentum-scrolling"));
 const postcss_no_important = require(require.resolve('postcss-no-important'))({});
 const postcss_pxtorem = require(require.resolve("postcss-pxtorem"))({
   rootValue: 75,
@@ -25,14 +25,14 @@ const postcss_plugins = process.env.NODE_ENV !== "production" ?
 (abcJSON.wap ? [
   postcss_preset,
   postcss_clean,
-  postcss_momentum_scrolling,
+  // postcss_momentum_scrolling,
   postcss_no_important,
   //postcss_uncss,
   postcss_pxtorem
 ] : [
   postcss_preset,
   postcss_clean,
-  postcss_momentum_scrolling,
+  // postcss_momentum_scrolling,
   postcss_no_important,
   //postcss_uncss
 ]);
