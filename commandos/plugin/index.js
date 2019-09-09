4351621ac2bc81920804bc34aff7c6bea81651ed
YiJun <cloudywolf1@qq.com>
2019-09-10 01:14:35 +0800

    内置插件补充abcx.json

--- commandos/plugin/index.js
+++ commandos/plugin/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const { prompt } = require('enquirer');
 const struct = require('ax-struct-js');
 
--- core/templates/index.js
+++ core/templates/index.js
@@ -1,9 +1,18 @@
+const colors = require('colors');
 
 module.exports = {
   "[XCLI] Built-in Templates": {
-    "[SPA] [Single Page Application] React+(TS|ES6)": "DemonCloud/cubec",
-    "[MPA] [Multiple Page Application] React+(TS|ES6)": "DemonCloud/cubec",
+    "[SPA] [Single Page Application] React+(TS|ES6)+PUG": {
+      "[SPA] Typescript Template": "DemonCloud/cubec",
+      "[SPA] JavaScript Template": "DemonCloud/cubec",
+    },
+    "[MPA] [Multiple Page Application] React+(TS|ES6)+PUG": {
+      "[MPA] Typescript Template": "DemonCloud/cubec",
+      "[MPA] JavaScript Template": "DemonCloud/cubec",
+    },
     // "[MPWA] [Multiple Modules Progressed Web Application] React+(TS|ES6)+PUG": "DemonCloud/cubec"
   },
-  "[XCLI] Implements Plugin": "DemonCloud/xcli-plugin-template"
+  "[XCLI] Implements Plugin": {
+    [`[XCLI] Plugin base on ${"abcx(1.0)".bold}`]: "DemonCloud/xcli-plugin-template"
+  }
 };
