5165c98f24c2596b6fd3f5c3aa52da41e9d8c415
YiJun <cloudywolf1@qq.com>
2019-09-15 16:40:57 +0800

    Upgrade builtin package
    Upgrade builtin package

--- core/common/tools/checkPluginExist.js
+++ core/common/tools/checkPluginExist.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const paths = require('../../utils/paths');
+const { isString } = require('lodash');
 
 module.exports = function(pluginType, findBuiltIn=false){
-  if(!pluginType) return false;
+  if(!pluginType || !isString(pluginType)) return false;
 
   return fs.existsSync(path.resolve(paths.pluginsPath, pluginType)) || (findBuiltIn ?
          fs.existsSync(path.resolve(paths.pluginsBuiltinPath, pluginType)) : false);
