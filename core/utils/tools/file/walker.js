7d3151f583a6cda1cd7a081e9ee3a529037a60bc
YiJun <cloudywolf1@qq.com>
2019-09-17 18:13:34 +0800

    async await file walker

--- core/utils/tools/file/walker.js
+++ core/utils/tools/file/walker.js
@@ -1,9 +1,43 @@
 const fs = require('fs');
+const util = require('util');
 const path = require('path');
+const struct = require('ax-struct-js');
+
+const map = struct.map();
+const isFn = struct.type('func');
+
+const fsreaddir = util.promisify(fs.readdir);
+const fsstat = util.promisify(fs.stat);
 
 // 查找目录下所有的文件
 // 不包括目录，纯文件
-const filewalker = function(dir, done) {
+const filewalker = async function(dir) {
+  let results = [];
+
+  const dirs = await fsreaddir(dir);
+
+  if(!dirs || !dirs.length) return false;
+
+  const childWalker = map(dirs, async function(file){
+    const filePath = path.resolve(dir, file);
+    const stats = await fsstat(filePath);
+
+    if(stats && stats.isDirectory()){
+      const childResults = await filewalker(filePath);
+      return childResults;
+    }
+
+    return [filePath];
+  });
+
+  const getChildFiles = await Promise.all(childWalker);
+
+  results = results.concat.apply(results, getChildFiles);
+
+  return results;
+};
+
+const syncFilewalker = function(dir, done) {
   let results = [];
 
   fs.readdir(dir, function(err, list) {
@@ -18,9 +52,11 @@ const filewalker = function(dir, done) {
       file = path.resolve(dir, file);
 
       fs.stat(file, function(err, stat) {
+        if(err) return done(err);
+
         if (stat && stat.isDirectory()) {
 
-          filewalker(file, function(err, res) {
+          syncFilewalker(file, function(err, res) {
             results = results.concat(res);
             if (!--pending) done(results);
           });
@@ -39,4 +75,9 @@ const filewalker = function(dir, done) {
   });
 };
 
-module.exports = filewalker;
+module.exports = function(dir, done){
+  if(done && isFn(done))
+    return syncFilewalker(dir, done);
+
+  return filewalker(dir);
+};
