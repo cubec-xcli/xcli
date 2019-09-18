20526ba7211bea86bb3ebc5af3c2038bf61118bb
YiJun <cloudywolf1@qq.com>
2019-09-18 13:04:32 +0800

    export builtin modules

--- core/utils/tools/index.js
+++ core/utils/tools/index.js
@@ -3,10 +3,22 @@ const walker = require('./file/walker');
 const openBrowser = require('./system/openBrowser');
 const optimizeCssModulesPlugin = require('./system/optimizeCssModulesPlugin');
 
+// modules
+const enquirer = require('./modules/enquirer');
+const fsextra = require('./modules/fsextra');
+const struct = require('./modules/struct');
+const axios = require('./modules/axios');
+
 module.exports = {
   file: {
     existDir,
     walker,
+  },
+  modules: {
+    enquirer,
+    fsextra,
+    struct,
+    axios,
   },
   system: {
     openBrowser,
---
+++ core/utils/tools/modules/struct.js
@@ -0,0 +1,2 @@
+module.exports = require('ax-struct-js');
+
