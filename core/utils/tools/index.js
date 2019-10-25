9b37959565de98efdda6551d0bf10c59b8b661af
YiJun <cloudywolf1@qq.com>
2019-10-25 16:36:05 +0800

    更新xcli包版本，增加date-fns作为内置时间转化工具

--- core/utils/tools/index.js
+++ core/utils/tools/index.js
@@ -3,6 +3,7 @@ const existDir = require('./file/existDir');
 const walker = require('./file/walker');
 // modules
 const enquirer = require('./modules/enquirer');
+const datefns = require('./modules/datefns');
 const fsextra = require('./modules/fsextra');
 const struct = require('./modules/struct');
 const lodash = require('./modules/lodash');
@@ -22,6 +23,8 @@ module.exports = {
   modules: {
     enquirer,
     fsextra,
+    datefns,
+    "date-fns": datefns,
     struct,
     lodash,
     axios,
